4e43e21234dd9a10a9b191bd3acd3444fbedf117
Bo-zhou4900 <zhoubo1233@hotmail.com>
2022-09-12 13:20:42 -0700

    Fix code style by Google style

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,13 @@
 package org.comp2522;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("My favorite animal is cat.");
-    }
+/**
+ * Comp 2522 lab 01 assignment.
+ *
+ * @author Bo Zhou
+ * @version 1.0
+ * @since 2022-09-12
+ */
+public class Main { public static void main(String[] args) {
+    System.out.println("My favorite animal is cat.");
+  }
 }
